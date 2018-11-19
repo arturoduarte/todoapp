6bec300e5277204b71551c199efcdc5c3f899e3d
arturo <informa.fj@gmail.com>
2018-11-18 22:49:01 -0300

    commit prueba desde arturo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # modificado desde la rama de development
-  
+  def develpment_arturo
+    
+  end
 end
