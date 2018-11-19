8ef40c0d473e2c6fb6f6b3eda7e2d9e71d8110e5
arturo <informa.fj@gmail.com>
2018-11-18 22:12:19 -0300

    eliminacion de metodo en application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # modificado desde la rama de development
-
-  def method_name
-    valor = 1
-    resultado = valor + 1
-  end
 end
