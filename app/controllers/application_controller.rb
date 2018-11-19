551cc5e0d05088ae0f746651bc1aa5eeffefa344
arturo <informa.fj@gmail.com>
2018-11-18 22:42:34 -0300

    limpieza de applicaition_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # modificado desde la rama de development
-  def pronombre
-    
-  end
-
-
-
-  def apellido
-    puts "apellido"
-  end
+  
 end
